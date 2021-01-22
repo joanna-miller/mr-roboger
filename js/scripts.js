97b750dcd5bd1b36016772845efccad3549c59ce
Jo <joannadawnmiller@gmail.com>
2021-01-22 13:19:46 -0500

    still working on test 4

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,8 @@
-let userInput = 1;
+let userInput = 4;
 const returnArray = [];
 
 function mrRobogers(userInput) {
   for (let index = 0; index <= userInput; index ++) {
     returnArray.push(index);
-    console.log(returnArray);
   }
 }
