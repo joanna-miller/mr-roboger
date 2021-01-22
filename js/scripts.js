697d31dc6fec8ac7e96d6b69230ba18019e70574
Jo <joannadawnmiller@gmail.com>
2021-01-22 15:01:28 -0500

    work on test 5

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,12 @@
 let userInput = 4;
 const userInputArray = [];
 
+
 function mrRobogers(userInput) {
   for (let index = 0; index <= userInput; index ++) {
     userInputArray.push(index);  
   }
-  const returnString = userInputArray.join(" ");
+  let returnString = userInputArray.join(" ");
   console.log(returnString.replace(/3/g, "Won't you be my neighbor?"));
-}
+};
 
