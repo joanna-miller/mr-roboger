e22ee9c74c749455ddb08dcc771b7a2a83fb1789
Jo <joannadawnmiller@gmail.com>
2021-01-22 16:43:51 -0500

    succesfully completed test 3, I believe correctly this time

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,13 @@
-let userInput = 13;
+let userInput = 3;
 const userInputArray = [];
 
 function mrRobogers(userInput) {
   for (let index = 0; index <= userInput; index ++) {
     userInputArray.push(index);  
+  if (userInputArray.includes(3)) {
+    userInputArray.pop();
+    userInputArray.push("Won't you be my neighbor?");    
+    }
   }
 };
 
