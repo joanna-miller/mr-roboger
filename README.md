fb0817ea774424c4ac940703fc33fb91a21269bb
Jo <joannadawnmiller@gmail.com>
2021-01-22 18:09:06 -0500

    stil lworking on test 5

--- README.md
+++ README.md
@@ -38,10 +38,12 @@ Test: It should return an array of values from user input.
 Expect: (mrRobogers(1).toEqual([0,1]));
 
 Test: It should return an array of values from user input, with 3 to be replaced with "Won't you be my neighbor?".
-Expect: (mrRobogers(3).toEqual([0, 1, 2, Won't you be my neighbor?]) 
+Expect: (mrRobogers(3).toEqual([0, 1, 2, "Won't you be my neighbor?"]) 
 
-Test: It should return an array of values from user input, with 2 to be replaced with "Boop!".
-Expect (mrRobogers(3).toEqual([0, 1, Boop!, Won't you be my neighbor?]))
+Test: It should return an array of values from user input, with any number including 3 to be replaced with "Won't you be my neighbor?".
+Expect (mrRobogers(13).toEqual([0, 1, 2, "Won't you be my neighbor?", 4, 5, 6, 7, 8, 9, 10, 11, 12, "Won't you be my neighbor?"]))
+
+
 
 ## License
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,47 @@
-let userInput = 13;
-const stringInput = userInput.toString();
-const userInputArray = [];
+let userInput = 15;
+let userInputArray = [];
+let stringInput = [];
+
 
 function mrRobogers(userInput) {
  
   for (let index = 0; index <= userInput; index ++) {
     userInputArray.push(index);  
+  }
+  userInputArray.join().split(",");
+  for (let index = 0; index < userInputArray.length - 1; index ++) {
   if (userInputArray.includes(3)) {
     userInputArray.pop();
-    userInputArray.push("Won't you be my neighbor?");    
-    }
+    userInputArray.push("Won't you be my neighbor?");   
   }
+  } 
+  
 };
 
+
+/* function createString(array) {
+  array.join().split(",");
+  for (let index = 0; index < array.length - 1; index ++) {
+    if (array.includes(element)) {
+    array.pop();
+    array.push("");   
+    } 
+   } */
+
+
+
+
+/* function mrRobogers(userInput) {
+ 
+  for (let index = 0; index <= userInput; index ++) {
+    userInputArray.push(index);  
+    if (userInputArray.includes(3)) {
+    userInputArray.pop();
+    userInputArray.push("Won't you be my neighbor?");    
+    } 
+  if (userInputArray.includes(2)) {
+    userInputArray.pop();
+    userInputArray.push("Boop!");    
+    }
+  }
+}; */ 
